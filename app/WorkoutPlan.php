b5f0b337b82409ce9380c10bb311be2b51a5a210
Helen Odia <helenodia@gmail.com>
2019-03-27 17:52:40 +0000

    tidied files

--- app/WorkoutPlan.php
+++ app/WorkoutPlan.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class WorkoutPlan extends Model
 {
-    //
+    // protected $fillable = ["title", "description"];
+    // protected $with = ["categories"];
+
+    // public function categories()
+    // {
+    //     return $this->belongsToMany(Category::class);
+    // }
+
+    // public function setCategories(Collection $categories)
+    // {
+    // 	//update pivot table with cat ids
+    // 	$this->categories()->sync($categories->pluck('id')->all());
+    // 	return $this;
+    // }
+
+	
 }
